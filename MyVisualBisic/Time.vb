7ef8cd2a039a2ec2763bc05033258b3bd6b821a2
月之领主LM <178910432@qq.com>
2017-06-06 03:10:42 +0800

    Update

---
+++ MyVisualBisic/Time.vb
@@ -0,0 +1,34 @@
+﻿Namespace My
+    
+    ''' <summary>
+    ''' 时间管理、转换相关函数
+    ''' </summary>
+    ''' <remarks></remarks>
+    Public NotInheritable Class Time
+
+        ''' <summary>
+        ''' 将当前线程挂起指定的时间（System.Threading.Thread.Sleep）
+        ''' </summary>
+        ''' <param name="Second">等待时间（单位秒，必须为非负值）</param>
+        ''' <remarks></remarks>
+        Public Shared Sub Wait(ByVal Second As Double)
+            If Second < 0 Then
+                Return
+            End If
+            System.Threading.Thread.Sleep(Second * 1000)
+        End Sub
+
+
+
+        ''' <summary>
+        ''' 获取UNIX时间戳
+        ''' </summary>
+        ''' <returns>UNIX时间戳整数</returns>
+        ''' <remarks></remarks>
+        Public Shared Function Stamp() As Long
+            Return (Now.ToUniversalTime() - New DateTime(1970, 1, 1, 0, 0, 0, 0)).TotalSeconds()
+        End Function
+
+    End Class
+
+End Namespace
